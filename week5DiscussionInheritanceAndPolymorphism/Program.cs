fb84cdc6129923d9bda014a9bc8d19a6e3b429c3
jaredsylvia <jaredsylvia@gmail.com>
2022-08-02 16:40:00 -0700

    Create Program.cs

---
+++ week5DiscussionInheritanceAndPolymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace inheritanceExample
+{
+
+    class SoftDrinkBrand
+    {
+        public string? brand { get; set; }
+
+        public void displayDetails()
+        {
+            Console.WriteLine("{0} is the brand", this.brand);
+        }
+    }
+    class ProductLine : SoftDrinkBrand
+    {
+        public string? line { get; set; }
+        public void displayDetails()
+        {
+            Console.WriteLine("{0} is made by {1}", this.line, this.brand);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftDrinkBrand CocaCola = new SoftDrinkBrand { brand = "Coca-Cola" };
+            CocaCola.displayDetails();
+
+            ProductLine Fanta = new ProductLine
+            {
+                brand = "Coca-Cola",
+                line = "Fanta"
+            };
+            Fanta.displayDetails();
+        }
+    }
+}
